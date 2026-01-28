704113708bf94be7ebd87d94ee364642255ee599
subham <subhamomm32@gmail.com>
2026-01-28 17:07:40 +0530

    requirment.txt added

--- custom_cake/serializers.py
+++ custom_cake/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import CakeOrder
 
 class CakeOrderSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(read_only=True)
+    # image = serializers.ImageField(read_only=True)
 
     class Meta:
         model = CakeOrder
